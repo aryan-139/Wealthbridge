9488f730991551f09443cdf8c0955a9f2e155ae1
Aryan Raj <aryanraj2k25@gmail.com>
2023-06-14 12:49:15 +0530

    grid E hard coded values

--- client/src/scenes/dashboard/Row2.tsx
+++ client/src/scenes/dashboard/Row2.tsx
@@ -1,14 +1,19 @@
 import BoxHeader from '@/components/BoxHeader';
 import DashboardBox from '@/components/DashboardBox';
+import FlexBetween from '@/components/FlexBetween';
 import { useGetKpisQuery, useGetProductsQuery } from '@/state/api';
-import { useTheme } from '@mui/material';
+import { Box, Typography, useTheme } from '@mui/material';
 import React, { useMemo } from 'react';
-import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import { CartesianGrid, Cell, Legend, Line, LineChart, Pie, PieChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
-type Props = {};
+const piedata=[
+  { name:" Group A", value:600},
+  {name: "Group B", value:400},
+]
 
 const Row2 = (props: Props) => {
   const{palette}=useTheme();
+  const pieColors= [palette.primary[800],palette.primary[300]];
   const{data:operationalData}= useGetKpisQuery(); 
   const{data: productData}= useGetProductsQuery();
   console.log("data",operationalData);
@@ -86,8 +91,63 @@ const Row2 = (props: Props) => {
         </LineChart>
       </ResponsiveContainer>
     </DashboardBox>
-    
-    <DashboardBox  gridArea="e"></DashboardBox>
+     
+    <DashboardBox  gridArea="e">
+      <BoxHeader title="Campaigns and Targets" sideText="+4%" />
+      <FlexBetween mt="0.25rem" gap="1.5rem" pr="1rem">
+    <PieChart 
+      width={110} 
+      height={100}
+      margin={{
+        top: 0,
+        right: -10,
+        left: 10,
+        bottom: 0,
+      }}
+    >
+        <Pie
+          stroke="none"
+          data={piedata}
+          innerRadius={18}
+          outerRadius={38}
+          paddingAngle={2}
+          dataKey="value"
+        >
+          {piedata.map((entry, index) => (
+            <Cell 
+            key={`cell-${index}`} 
+            fill={pieColors[index]} />
+          ))}
+        </Pie>
+
+      </PieChart>
+      <Box ml="-0.7rem" flexBasis="40%" textAlign="center">
+        <Typography variant='h5'>Target Sales</Typography>
+        <Typography m="0.3rem 0" variant='h3' color={palette.primary[300]}>
+          83
+        </Typography>
+        <Typography variant='h6'>
+          Finance goals of the campaign that is desired
+        </Typography>
+      </Box>
+
+      <Box flexBasis="40%">
+        <Typography variant='h5'>Losses in Revenue</Typography>
+        <Typography variant='h6'>
+          Losses are down by 25%
+        </Typography>
+        <Typography mt="0.4rem" variant='h5'>
+          Profit margins
+        </Typography>
+        <Typography variant='h6'>
+          Margins are up by 30% from last month
+        </Typography>
+      </Box>
+
+      </FlexBetween>
+
+    </DashboardBox>
+
     <DashboardBox  gridArea="f"></DashboardBox>
     </>
   );
