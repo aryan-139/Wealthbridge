06123e2cbe40755cf018a22a38f567007e9a894b
Aryan Raj <aryanraj2k25@gmail.com>
2023-06-14 14:27:19 +0530

    grid F completed

--- client/src/scenes/dashboard/Row2.tsx
+++ client/src/scenes/dashboard/Row2.tsx
@@ -4,14 +4,14 @@ import FlexBetween from '@/components/FlexBetween';
 import { useGetKpisQuery, useGetProductsQuery } from '@/state/api';
 import { Box, Typography, useTheme } from '@mui/material';
 import React, { useMemo } from 'react';
-import { CartesianGrid, Cell, Legend, Line, LineChart, Pie, PieChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import { CartesianGrid, Cell, Legend, Line, LineChart, Pie, PieChart, ResponsiveContainer, Scatter, ScatterChart, Tooltip, XAxis, YAxis, ZAxis } from 'recharts';
 
-const piedata=[
+const pieData=[
   { name:" Group A", value:600},
   {name: "Group B", value:400},
 ]
 
-const Row2 = (props: Props) => {
+const Row2 = () => {
   const{palette}=useTheme();
   const pieColors= [palette.primary[800],palette.primary[300]];
   const{data:operationalData}= useGetKpisQuery(); 
@@ -30,6 +30,20 @@ const Row2 = (props: Props) => {
       })
     );
   }, [operationalData]);
+
+  const productExpenseData = useMemo(() => {
+    return (
+      productData &&
+      productData.map(
+        ({ _id, price, expense}) => {
+        return {
+          id: _id,
+          price: price,
+          expense: expense
+        };
+      })
+    );
+  }, [productData]);
 
   return (
     <>
@@ -107,13 +121,13 @@ const Row2 = (props: Props) => {
     >
         <Pie
           stroke="none"
-          data={piedata}
+          data={pieData}
           innerRadius={18}
           outerRadius={38}
           paddingAngle={2}
           dataKey="value"
         >
-          {piedata.map((entry, index) => (
+          {pieData.map((entry, index) => (
             <Cell 
             key={`cell-${index}`} 
             fill={pieColors[index]} />
@@ -148,7 +162,53 @@ const Row2 = (props: Props) => {
 
     </DashboardBox>
 
-    <DashboardBox  gridArea="f"></DashboardBox>
+
+
+    <DashboardBox  gridArea="f">
+    <BoxHeader title='Product Prices vs Expenses' sideText='+4%'/>
+    <ResponsiveContainer width="100%" height="100%">
+      
+        <ScatterChart
+          margin={{
+            top: 20,
+            right: 25,
+            bottom: 40,
+            left: -10,
+          }}
+        >
+          <CartesianGrid stroke={palette.grey[800]} />
+
+          <XAxis 
+          type="number" 
+          dataKey="price" 
+          name="price" 
+          axisLine={false}
+          tickLine={false}
+          style={{fontSize: "10px"}}
+          tickFormatter={(v)=> `$${v}`}
+          />
+          <YAxis 
+          type="number" 
+          dataKey="expense" 
+          name="expense" 
+          axisLine={false}
+          tickLine={false}
+          style={{fontSize: "10px"}}
+          tickFormatter={(v)=> `$${v}`}
+          />
+          <ZAxis type='number' range={[20]}          
+          />
+
+          <Tooltip formatter={(v)=> `$${v}` } />
+          <Scatter name="Product Expense Ratio" 
+          data={productExpenseData} 
+          fill={palette.tertiary[500]} 
+          />
+        </ScatterChart>
+      </ResponsiveContainer>
+
+
+    </DashboardBox>
     </>
   );
 };
